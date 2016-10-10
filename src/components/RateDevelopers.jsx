393ee5e6646959b30b62194bc3c4dddeb4ee039a
david <tdsemakula@gmail.com>
2016-10-10 08:34:44 +0300

    Add Done button to rate developers

--- src/components/RateDevelopers.jsx
+++ src/components/RateDevelopers.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Rating from 'react-rating';
 
-import { RATING_CRITERIA_CHOICES } from '../constants/Api';
+import {RATING_CRITERIA_CHOICES} from '../constants/Api';
 
 export default class RateDevelopers extends React.Component {
 
@@ -15,18 +16,18 @@ export default class RateDevelopers extends React.Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        if(nextProps.Task.detail.isSaved && !this.props.Task.detail.isSaved) {
+        if (nextProps.Task.detail.isSaved && !this.props.Task.detail.isSaved) {
             this.mapUserRatings(nextProps);
         }
     }
 
     mapUserRatings(props) {
-        const { Auth, Task } = props;
-        const { task } = Task.detail;
-        if(task && task.ratings && task.ratings.length) {
+        const {Auth, Task} = props;
+        const {task} = Task.detail;
+        if (task && task.ratings && task.ratings.length) {
             var ratings_map = {};
             task.ratings.forEach(rating => {
-                if(rating.created_by.id == Auth.user.id) {
+                if (rating.created_by.id == Auth.user.id) {
                     ratings_map[rating.criteria] = rating.score;
                 }
             });
@@ -35,7 +36,7 @@ export default class RateDevelopers extends React.Component {
     }
 
     onRatingChange(criteria, rating) {
-        const { TaskActions, Task } = this.props;
+        const {TaskActions, Task} = this.props;
         TaskActions.updateTask(Task.detail.task.id, {ratings: [{criteria, score: rating}]});
     }
 
@@ -47,30 +48,34 @@ export default class RateDevelopers extends React.Component {
 
     render() {
 
-        const { Task } = this.props;
-        const { task } = Task.detail;
+        const {Task} = this.props;
+        const {task} = Task.detail;
 
         return (
             <div>
                 <form onSubmit={this.handleSubmit.bind(this)} name="rate" role="form" ref="rate_form">
-                <h4 className="title">Rate Developer{task.details && task.details.participation && task.details.participation.length==1?'':'s'}</h4>
+                    <h4 className="title">Rate
+                        Developer{task.details && task.details.participation && task.details.participation.length == 1 ? '' : 's'}</h4>
 
-                <div className="card">
-                    It would help the Tunga community a great deal if you could rate our
-                    developers. You can rate a developer on three scales. Please take 5 seconds of your to rate our Developers. Thank you in advance
-                </div>
-                {RATING_CRITERIA_CHOICES.map(criteria => {
-                    return (
-                        <div key={criteria.id} style={{margin: '15px 0'}}>
-                            <h5>{criteria.name}</h5>
-                            <div className="rating large">
-                                <Rating start={0} stop={10} step={2} fractions={2} initialRate={this.state.ratings_map?this.state.ratings_map[criteria.id]:0}
-                                        empty={'fa fa-star-o'} full={'fa fa-star'}
-                                        onChange={this.onRatingChange.bind(this, criteria.id)}/>
+                    <div className="card">
+                        It would help the Tunga community a great deal if you could rate our
+                        developers. You can rate a developer on three scales. Please take 5 seconds of your to rate our
+                        Developers. Thank you in advance
+                    </div>
+                    {RATING_CRITERIA_CHOICES.map(criteria => {
+                        return (
+                            <div key={criteria.id} style={{margin: '15px 0'}}>
+                                <h5>{criteria.name}</h5>
+                                <div className="rating large">
+                                    <Rating start={0} stop={10} step={2} fractions={2}
+                                            initialRate={this.state.ratings_map?this.state.ratings_map[criteria.id]:0}
+                                            empty={'fa fa-star-o'} full={'fa fa-star'}
+                                            onChange={this.onRatingChange.bind(this, criteria.id)}/>
+                                </div>
                             </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                    <Link to="/" className="btn">Done</Link>
                 </form>
             </div>
         );
