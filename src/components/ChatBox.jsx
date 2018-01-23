c61b345b99275ce7d435ef15658b7efbbb0048a7
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2018-01-23 09:53:56 +0100

    Feat/add offline chat actions (#264)
    * style(offline-buttons): added stlye for buttons
    
    * feat(chatbox.jsx): refactored chatbox and added offline action buttons
    
    * style(chat-action): fixed missing class name syntax
    
    * feat(offline-chat-action): added state to manage display of showEmailForm

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Button} from 'react';
 import {Link} from 'react-router';
 import Avatar from './Avatar';
 
@@ -11,11 +11,99 @@ import {isAuthenticated} from '../utils/auth';
 import {CHANNEL_TYPES} from '../constants/Api';
 
 export default class ChatBox extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      showEmailForm: false
+    }
+  }
   getView() {
     if (this.props.channelView) {
       return this.props.channelView;
     }
     return null;
+  }
+
+  getEmailForm() {
+    const { Channel } = this.props
+    const emailForm = {
+      action: 'send',
+      activity_type: 'message',
+      activity: {
+        sender: {
+          id: 'tunga',
+          username: null,
+          short_name: 'Tunga',
+          display_name: 'Tunga',
+          avatar_url: 'https://tunga.io/icons/Tunga_squarex150.png',
+          hide: true,
+        },
+        isForm: true,
+        body: (
+          <div>
+            {Channel.detail.channel.object_id
+              ? <div className="text-center got-it">
+                  <div>We got it! Thanks</div>
+                  <i className="icon tunga-icon-check" />
+                </div>
+              : <div>
+                  <div>Where can we reach you to follow up?</div>
+                  <SupportChannelMiniForm {...this.props} />
+                </div>}
+          </div>
+        ),
+      },
+    };
+
+    return emailForm;
+  }
+
+  getInitMessage() {
+    const { channel } = this.props
+    return {
+      action: 'send',
+      activity_type: 'message',
+      activity: {
+        sender: {
+          id: 'tunga',
+          username: null,
+          short_name: 'Elijah',
+          display_name: 'Elijah',
+          avatar_url: require('../images/chat/elijah.jpg'),
+        },
+        body: 'Hi, feel free to ask me anything.',
+      }
+    };
+  }
+
+  getOfflineActionsActivity() {
+    return {
+      action: 'send',
+      activity_type: 'message',
+      activity: {
+        sender: {
+          id: 'tunga',
+          username: null,
+          short_name: 'Tunga',
+          display_name: 'Tunga',
+          avatar_url: require('../images/logo_round.png'),
+        },
+        isForm: true,
+        body: (
+          <div>
+            <p>Uh-oh, we are currently not online.</p>
+            <p>We will reach out to you ASAP!</p>
+            <div className="btn-group bubble-action" role="group">
+              <button type="button" className="btn btn-default pull-left">
+                <Link to='/call'>Schedule call </Link>
+              </button>
+              <button type="button" onClick={() => this.setState({showEmailForm: true})} className="btn btn-default pull-right">Contact me via email</button>
+            </div>
+          </div>
+        )
+        // body: 'Uh-oh, we are currently not online.\nWe will reach out to you ASAP!',
+      }
+    }
   }
 
   onSendMessage(body, attachments) {
@@ -39,56 +127,21 @@ export default class ChatBox extends React.Component {
       Channel.detail.activity.items[getChannelKey(channel.id)] || [];
     if (channel.type == CHANNEL_TYPES.support) {
       activities = [
-        {
-          action: 'send',
-          activity_type: 'message',
-          activity: {
-            sender: {
-              id: 'tunga',
-              username: null,
-              short_name: 'Elijah',
-              display_name: 'Elijah',
-              avatar_url: require('../images/chat/elijah.jpg'),
-            },
-            body: 'Hi, feel free to ask me anything.',
-          },
-        },
+        this.getInitMessage(),
         ...activities,
       ];
-
       if (activities.length >= 2 && !isAuthenticated()) {
-        let emailForm = {
-          action: 'send',
-          activity_type: 'message',
-          activity: {
-            sender: {
-              id: 'tunga',
-              username: null,
-              short_name: 'Tunga',
-              display_name: 'Tunga',
-              avatar_url: 'https://tunga.io/icons/Tunga_squarex150.png',
-              hide: true,
-            },
-            isForm: true,
-            body: (
-              <div>
-                {Channel.detail.channel.object_id
-                  ? <div className="text-center got-it">
-                      <div>We got it! Thanks</div>
-                      <i className="icon tunga-icon-check" />
-                    </div>
-                  : <div>
-                      <div>Where can we reach you to follow up?</div>
-                      <SupportChannelMiniForm {...this.props} />
-                    </div>}
-              </div>
-            ),
-          },
-        };
         activities = [
           ...activities.slice(0, 2),
-          emailForm,
+          this.getOfflineActionsActivity(),
           ...activities.slice(2),
+        ];
+      }
+      if (activities.length >= 3 && !isAuthenticated() && this.state.showEmailForm) {
+        activities = [
+          ...activities.slice(0, 3),
+          this.getEmailForm(),
+          ...activities.slice(3),
         ];
       }
     }
