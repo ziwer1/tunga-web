53cc205591d2a33309d9625bca732c37edc8a887
Wamos <wamoscode@gmail.com>
2018-04-04 13:15:48 +0300

    font fixes

--- src/routes/StoryPage.jsx
+++ src/routes/StoryPage.jsx
@@ -84,7 +84,7 @@ export default class StoryPage extends React.Component {
                 Our Dream: Equal Opportunity for All Youths
                 <hr className="under-line" style={{width:'683px'}}/>
               </div>
-              <p>
+              <p id="text">
                   Being able to take control of and choose your own destiny is a universal value. 
                   And it probably is disproportionately so among software developers and startup entrepreneurs 
                   like ourselves. Tunga is founded by not-for-profit design studio <a href="http://web.butterflyworks.org/">Butterfly Works </a> and long-time social entrepreneur Ernesto Spruyt, who together with his colleague Bart Leijssenaar (CMO) and Michiel Huisman (Board Advisor) not only is a tech person, but also has a background in hospitality management. In short: Tunga is founded by ‘people-people’. We think that what you make of your life is your own responsibility. But we would like for everyone, especially the youth that holds the world’s future, to be free and able to take control of their own destiny.
@@ -112,7 +112,7 @@ export default class StoryPage extends React.Component {
                 The Problem: African Youths Don’t Have The Same Opportunities
                 <hr className="under-line" style={{width:'683px'}}/>
               </div>
-              <p>
+              <p id="text">
               But the reality is that in many parts of the world, the opportunity 
               you have to determine your own destiny is limited by circumstances. 
               By factors outside of your own control. In our search for software developers, 
@@ -134,7 +134,7 @@ export default class StoryPage extends React.Component {
                 Our Mission: 21st Century Jobs for African Youths
                 <hr className="under-line" style={{width:'683px',}}/>
               </div>
-              <p>
+              <p id="text" >
               That’s why in 2015 we embarked on a mission to create 21st century jobs for African youths. With the support of institutions like Dioraphte, Oxfam, the DOEN Foundation, Edukans and Triodos Bank we started Tunga. To help talented African youths get access to the cool tech jobs that define this generation. So that they can become independent and make their mark in the international economic marketplace.
               </p>
 
@@ -198,7 +198,7 @@ export default class StoryPage extends React.Component {
                 International Teams and Projects
                 <hr className="under-line" style={{width:'683px'}}/>
               </div>
-              <p>
+              <p id="text">
               We believe in the power of business to transform society. And probably the most attractive area for creating high skilled jobs and skills development for today’s ambitious youths is software development. As production methods from the open source community are increasingly being adopted in the industry, software is often already being produced in virtual teams. At the same time, there is a huge shortage of software developers around the world, resulting in an infinite demand for software development skills. So Tunga was set up as a platform to remove all barriers for these youths to participate in international software teams and projects.
               </p>
               <div className="video-wrapper">
@@ -218,7 +218,7 @@ export default class StoryPage extends React.Component {
                 Impact Sourcing - (Software) Outsourcing Done Right
                 <hr className="under-line" style={{width:'683px'}}/>
               </div>
-              <p>
+              <p id="text">
               Tunga is not a traditional software outsourcing platform. Our way of working is called Impact Sourcing: the deployment of workers from low-employment areas into the processes of businesses worldwide either through outsourcing or by setting up remote or virtual teams using digital technology. It aims to provide higher-income employment and access to new income opportunities to workers that otherwise might not be employed in that particular sector. For example, people in rural areas or in slums, people without access to education or educated individuals in areas of high unemployment.
               </p>
 
@@ -238,7 +238,7 @@ export default class StoryPage extends React.Component {
                 Why Africa: Tackling Huge Youth Unemployment
                 <hr className="under-line" style={{width:'683px'}}/>
               </div>
-              <p>
+              <p id="text">
 
                 Tunga’s clients come from all over the world, but our developers exclusively come from Africa.
                 At this moment mostly from Uganda, Nigeria and Egypt. We started operations in Uganda and that’s
@@ -262,7 +262,7 @@ export default class StoryPage extends React.Component {
                 Our Human-Centered Approach Cuts Both Ways
                 <hr className="under-line" style={{width:'683px'}}/>
               </div>
-              <p>
+              <p id="text">
                Our way of working allows us to do software outsourcing better than traditional players in the market. 
                Users of outsourcing services are generally complaining about 2 things: quality and communication.
                Tunga works with highly motivated and loyal developers who are eager to learn and understand that 
