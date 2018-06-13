798df98c0bd9583f611ef8a13f20959f3ca2eeaf
david <tdsemakula@gmail.com>
2018-06-13 08:51:51 +0300

    add reactstrap and fix avatar tooltip

--- src/components/core/Avatar.jsx
+++ src/components/core/Avatar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
-import {OverlayTrigger, Tooltip} from 'react-bootstrap';
+import {Tooltip} from 'reactstrap';
+import randomstring from 'randomstring';
 
 export default class Avatar extends React.Component {
     static propTypes = {
@@ -13,10 +14,22 @@ export default class Avatar extends React.Component {
         badge: React.PropTypes.number,
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {tooltipOpen: false};
+    }
+
+    toggle() {
+        this.setState({
+            tooltipOpen: !this.state.tooltipOpen
+        });
+    }
+
     render() {
-        const {className, image, icon, size, title, link, badge} = this.props;
+        const {className, image, icon, size, title, link, badge} = this.props,
+            avatarId = `avatar${randomstring.generate()}`;
         let avatar = (
-            <div className={`avatar-wrapper ${className || ''}`}>
+            <div>
                 {badge ? <span className="badge">{badge}</span> : null}
                 <div
                     className={`avatar ${size?`avatar-${size}`:''} ${image?'':'avatar-icon'}`}
@@ -31,15 +44,16 @@ export default class Avatar extends React.Component {
         let linkedAvatar = link ? <Link to={link}>{avatar}</Link> : avatar;
 
         return title ? (
-            <OverlayTrigger
-                placement="top"
-                overlay={
-                    <Tooltip id="tooltip">
-                        <strong>{title}</strong>
-                    </Tooltip>
-                }>
+            <div id={avatarId} className={`avatar-wrapper ${className || ''}`}>
                 {linkedAvatar}
-            </OverlayTrigger>
+                <Tooltip
+                    placement="top"
+                    target={avatarId}
+                    isOpen={this.state.tooltipOpen}
+                    toggle={this.toggle.bind(this)}>
+                    <strong>{title}</strong>
+                </Tooltip>
+            </div>
         ) : (
             linkedAvatar
         );
