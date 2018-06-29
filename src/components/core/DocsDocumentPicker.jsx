8e8111a872e52430f729f32b59d2d04b5ba0bd78
ziwer1 <ziwer1@gmail.com>
2018-06-29 14:27:49 +0300

    make readable

--- src/components/core/DocsDocumentPicker.jsx
+++ src/components/core/DocsDocumentPicker.jsx
@@ -1,29 +1,30 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 
-import IconButton from './IconButton';
-import DocumentType from './DocumentType';
+import IconButton from "./IconButton";
+import DocumentType from "./DocumentType";
 import DocumentForm from "./DocumentForm";
 
-import {openModal} from './utils/modals';
-import {addPropsToChildren} from './utils/children';
+import { openModal } from "./utils/modals";
+import { addPropsToChildren } from "./utils/children";
 import Button from "./Button";
 import Icon from "./Icon";
-import {DOCUMENT_TYPES} from "../../actions/utils/api";
+import { DOCUMENT_TYPES } from "../../actions/utils/api";
 
-const ModalWrapper = (props) => {
+const ModalWrapper = props => {
     return (
         <div>
-            {addPropsToChildren(props.children, {onChange: props.proceed, onSave: props.proceed})}
+            {addPropsToChildren(props.children, {
+                onChange: props.proceed,
+                onSave: props.proceed
+            })}
         </div>
     );
 };
 
 export default class DocsDocumentPicker extends React.Component {
-
     static defaultProps = {
-        variant: 'icon',
-
+        variant: "icon"
     };
 
     static propTypes = {
@@ -31,78 +32,110 @@ export default class DocsDocumentPicker extends React.Component {
         className: PropTypes.string,
         onChange: PropTypes.func,
         documentType: PropTypes.string,
-        documentTypes: PropTypes.array,
+        documentTypes: PropTypes.array
     };
 
     constructor(props) {
         super(props);
-
     }
 
-    filterDocumentsByType(docs, type){
+    filterDocumentsByType(docs, type) {
         let filtered_docs = [];
-        let len = docs.length
+        let len = docs.length;
 
         for (var i = 0; i < len; i++) {
-            if(docs[i].type === type){
-                filtered_docs.push(docs[i])
+            if (docs[i].type === type) {
+                filtered_docs.push(docs[i]);
             }
         }
 
-        return filtered_docs
+        return filtered_docs;
     }
 
     onSelectType = () => {
         openModal(
             <ModalWrapper>
-                <DocumentType/>
+                <DocumentType />
             </ModalWrapper>,
-            'Type of document',
-            true, {className: 'modal-upload'}
-         ).then((type) => {
-             this.onSelectDocument(type);
-         });
+            "Type of document",
+            true,
+            { className: "modal-upload" }
+        ).then(type => {
+            this.onSelectDocument(type);
+        });
     };
 
     onSelectDocument(type) {
         let self = this;
         openModal(
             <ModalWrapper>
-                <DocumentForm type={type} documentType={this.props.documentType} documentTypes={this.props.documentTypes}/>
+                <DocumentForm
+                    type={type}
+                    documentType={this.props.documentType}
+                    documentTypes={this.props.documentTypes}
+                />
             </ModalWrapper>,
-            `Add ${this.props.documentType?(DOCUMENT_TYPES[this.props.documentType] || `${this.props.documentType} document`):' document'}`,
-            true, {className: 'modal-upload'}
-        ).then((document) => {
-            this.props.ProjectActions.createDocument(document)
+            `Add ${
+                this.props.documentType
+                    ? DOCUMENT_TYPES[this.props.documentType] ||
+                      `${this.props.documentType} document`
+                    : " document"
+            }`,
+            true,
+            { className: "modal-upload" }
+        ).then(document => {
+            this.props.ProjectActions.createDocument(document);
         });
     }
 
     onRemoveDoc(idx) {
-        this.props.ProjectActions.deleteDocument(idx)
+        this.props.ProjectActions.deleteDocument(idx);
     }
 
     render() {
-        let filtered_docs= this.filterDocumentsByType(this.props.project.documents, this.props.documentType)
+        let filtered_docs = this.filterDocumentsByType(
+            this.props.project.documents,
+            this.props.documentType
+        );
 
         return (
             <div className="document-input">
-                {filtered_docs.length > 0?(
+                {filtered_docs.length > 0 ? (
                     <div className="file-list">
-                        {filtered_docs.map((doc) => {
+                        {filtered_docs.map(doc => {
                             return (
                                 <div className="file-item" key={doc.id}>
-                                    <a href={doc.download_url}><i className={doc.file?"tg-ic-download":"tg-ic-link"}/> {doc.file?doc.title:doc.url}</a>
-                                    <button className="btn" onClick={this.onRemoveDoc.bind(this, doc.id)}><i className="tg-ic-close"/></button>
+                                    <a href={doc.download_url}>
+                                        <i
+                                            className={
+                                                doc.file
+                                                    ? "tg-ic-download"
+                                                    : "tg-ic-link"
+                                            }
+                                        />{" "}
+                                        {doc.file ? doc.title : doc.url}
+                                    </a>
+                                    <button
+                                        className="btn"
+                                        onClick={this.onRemoveDoc.bind(
+                                            this,
+                                            doc.id
+                                        )}
+                                    >
+                                        <i className="tg-ic-close" />
+                                    </button>
                                 </div>
-                            )
+                            );
                         })}
                     </div>
-                ):null}
+                ) : null}
 
-                {this.props.variant === 'button'?(
-                    <Button onClick={this.onSelectType}><Icon name="upload"/> Add documents</Button>
-                ):(
-                    <IconButton name="add" onClick={this.onSelectType}/>
+                {this.props.variant === "button" ? (
+                    <Button onClick={this.onSelectType}>
+                        <Icon name="upload" /> Add documents
+                    </Button>
+                ) : (
+                    <IconButton name="add" onClick={this.onSelectType} />
                 )}
             </div>
         );
--- src/components/projects/Docs.jsx
+++ src/components/projects/Docs.jsx
@@ -1,14 +1,14 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 import { Container, Row, Col } from "reactstrap";
 
 import DocsDocumentPicker from "../core/DocsDocumentPicker";
-import {DOCUMENT_TYPES} from "../../actions/utils/api";
+import { DOCUMENT_TYPES } from "../../actions/utils/api";
 
 export default class Docs extends React.Component {
     static propTypes = {
         project: PropTypes.object,
-        ProjectActions: PropTypes.object,
+        ProjectActions: PropTypes.object
     };
 
     constructor(props) {
@@ -18,30 +18,78 @@ export default class Docs extends React.Component {
     render() {
         return (
             <div>
-                <br/>
+                <br />
                 <Container>
                     <Row className="documents-row-bottom-margin">
                         <Col>
-                            <div className='font-weight-medium'> Estimates document </div>
-                            <DocsDocumentPicker {...this.props} documentType={Object.keys(DOCUMENT_TYPES)[Object.keys(DOCUMENT_TYPES).indexOf('estimate')]}/>
+                            <div className="font-weight-medium">
+                                {" "}
+                                Estimates document{" "}
+                            </div>
+                            <DocsDocumentPicker
+                                {...this.props}
+                                documentType={
+                                    Object.keys(DOCUMENT_TYPES)[
+                                        Object.keys(DOCUMENT_TYPES).indexOf(
+                                            "estimate"
+                                        )
+                                    ]
+                                }
+                            />
                         </Col>
                     </Row>
                     <Row className="documents-row-bottom-margin">
                         <Col>
-                            <div className='font-weight-medium'> Proposal documents </div>
-                            <DocsDocumentPicker {...this.props} documentType={Object.keys(DOCUMENT_TYPES)[Object.keys(DOCUMENT_TYPES).indexOf('proposal')]}/>
+                            <div className="font-weight-medium">
+                                {" "}
+                                Proposal documents{" "}
+                            </div>
+                            <DocsDocumentPicker
+                                {...this.props}
+                                documentType={
+                                    Object.keys(DOCUMENT_TYPES)[
+                                        Object.keys(DOCUMENT_TYPES).indexOf(
+                                            "proposal"
+                                        )
+                                    ]
+                                }
+                            />
                         </Col>
                     </Row>
                     <Row className="documents-row-bottom-margin">
                         <Col>
-                            <div className='font-weight-medium'> Requirements documents </div>
-                            <DocsDocumentPicker {...this.props} documentType={Object.keys(DOCUMENT_TYPES)[Object.keys(DOCUMENT_TYPES).indexOf('requirements')]}/>
+                            <div className="font-weight-medium">
+                                {" "}
+                                Requirements documents{" "}
+                            </div>
+                            <DocsDocumentPicker
+                                {...this.props}
+                                documentType={
+                                    Object.keys(DOCUMENT_TYPES)[
+                                        Object.keys(DOCUMENT_TYPES).indexOf(
+                                            "requirements"
+                                        )
+                                    ]
+                                }
+                            />
                         </Col>
                     </Row>
                     <Row className="documents-row-bottom-margin">
                         <Col>
-                            <div className='font-weight-medium'> Other documents </div>
-                            <DocsDocumentPicker {...this.props} documentType={Object.keys(DOCUMENT_TYPES)[Object.keys(DOCUMENT_TYPES).indexOf('other')]}/>
+                            <div className="font-weight-medium">
+                                {" "}
+                                Other documents{" "}
+                            </div>
+                            <DocsDocumentPicker
+                                {...this.props}
+                                documentType={
+                                    Object.keys(DOCUMENT_TYPES)[
+                                        Object.keys(DOCUMENT_TYPES).indexOf(
+                                            "other"
+                                        )
+                                    ]
+                                }
+                            />
                         </Col>
                     </Row>
                 </Container>
