1154fca762ba9d693382b1d0355d73ae25506ca0
ziwer1 <ziwer1@gmail.com>
2018-06-29 14:16:19 +0300

    add Docs Document picker to Docs component

--- src/components/projects/Docs.jsx
+++ src/components/projects/Docs.jsx
@@ -1,5 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Container, Row, Col } from "reactstrap";
+
+import DocsDocumentPicker from "../core/DocsDocumentPicker";
+import {DOCUMENT_TYPES} from "../../actions/utils/api";
 
 export default class Docs extends React.Component {
     static propTypes = {
@@ -7,10 +11,40 @@ export default class Docs extends React.Component {
         ProjectActions: PropTypes.object,
     };
 
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div>
-                // TODO: {this.constructor.name} form goes here
+                <br/>
+                <Container>
+                    <Row className="documents-row-bottom-margin">
+                        <Col>
+                            <div className='font-weight-medium'> Estimates document </div>
+                            <DocsDocumentPicker {...this.props} documentType={Object.keys(DOCUMENT_TYPES)[Object.keys(DOCUMENT_TYPES).indexOf('estimate')]}/>
+                        </Col>
+                    </Row>
+                    <Row className="documents-row-bottom-margin">
+                        <Col>
+                            <div className='font-weight-medium'> Proposal documents </div>
+                            <DocsDocumentPicker {...this.props} documentType={Object.keys(DOCUMENT_TYPES)[Object.keys(DOCUMENT_TYPES).indexOf('proposal')]}/>
+                        </Col>
+                    </Row>
+                    <Row className="documents-row-bottom-margin">
+                        <Col>
+                            <div className='font-weight-medium'> Requirements documents </div>
+                            <DocsDocumentPicker {...this.props} documentType={Object.keys(DOCUMENT_TYPES)[Object.keys(DOCUMENT_TYPES).indexOf('requirements')]}/>
+                        </Col>
+                    </Row>
+                    <Row className="documents-row-bottom-margin">
+                        <Col>
+                            <div className='font-weight-medium'> Other documents </div>
+                            <DocsDocumentPicker {...this.props} documentType={Object.keys(DOCUMENT_TYPES)[Object.keys(DOCUMENT_TYPES).indexOf('other')]}/>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
