1e56b7174a2881b8f6de9b0fcd19cbedbe89a6ca
ziwer1 <ziwer1@gmail.com>
2018-07-01 13:19:54 +0300

    add docs component

--- src/components/projects/Docs.jsx
+++ src/components/projects/Docs.jsx
@@ -1,5 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Container, Row, Col } from "reactstrap";
+
+import DocumentPicker from "../core/DocumentPicker";
+import {DOCUMENT_TYPES} from "../../actions/utils/api";
 
 export default class Docs extends React.Component {
     static propTypes = {
@@ -7,11 +11,115 @@ export default class Docs extends React.Component {
         ProjectActions: PropTypes.object,
     };
 
+    constructor(props) {
+        super(props);
+        /*this.state = {
+            project: this.props.project,
+        };*/
+        this.documents={
+            estimate:[],
+            proposal:[],
+            requirements:[],
+            other:[],
+        }
+    }
+
+    /*componentDidUpdate(prevProps) {
+        if (prevProps.project.documents.length !== this.props.project.documents.length) {
+            this.setState({ project: {...this.props.project} });
+        }
+    }*/
+
+    
+
+    onChangeValue(key, value) {
+        let new_doc
+        
+        if (value.length > this.documents[key].length) {
+            new_doc = value[value.length - 1];
+            //this.props.ProjectActions.createDocument(new_doc)
+            console.log(new_doc)
+        }
+
+        this.documents[key] = [...value];
+
+    }
+
+    filterDocumentsByType(docs, type){
+        let filtered_docs = [];
+        let len = docs.length
+
+        for (var i = 0; i < len; i++) {
+            if(docs[i].type === type){
+                filtered_docs.push(docs[i])
+            }
+        }
+
+        return filtered_docs
+    }
+
+    renderfilteredDocs(type){
+
+        let filtered_docs= this.filterDocumentsByType(this.props.project.documents, type)
+        let list = []
+
+        if (filtered_docs.length > 0){
+            
+            list =  filtered_docs.map((doc) => {
+                    return (
+                        <div className="file-item" key={doc.id}>
+                            <a href={doc.download_url}><i className={doc.file?"tg-ic-download":"tg-ic-link"}/> {doc.file?doc.title:doc.url}</a>
+                            <button className="btn" onClick={this.onRemoveDoc.bind(this, doc.id)}><i className="tg-ic-close"/></button>
+                        </div>
+                    )
+                })
+
+            return <div className="file-list">{list}</div>
+        }
+
+            return <div className="file-list"></div>
+    }
+
+    onRemoveDoc(idx) {
+        this.props.ProjectActions.deleteDocument(idx)
+    }
+
     render() {
         return (
             <div>
-                // TODO: {this.constructor.name} form goes here
+                <br/>
+                <Container>
+                    <Row className="documents-row-bottom-margin">
+                        <Col>
+                            <div className='font-weight-medium'> Estimates document </div>
+                            {this.renderfilteredDocs('estimate')}
+                            <DocumentPicker showSelected={false} documentType='estimate' onChange={(docs) => { this.onChangeValue('estimate', docs)}}/>
+                        </Col>
+                    </Row>
+                    <Row className="documents-row-bottom-margin">
+                        <Col>
+                            <div className='font-weight-medium'> Proposal documents </div>
+                            {this.renderfilteredDocs('proposal')}
+                            <DocumentPicker showSelected={false} documentType='proposal' onChange={(docs) => { this.onChangeValue('proposal', docs)}}/>
+                        </Col>
+                    </Row>
+                    <Row className="documents-row-bottom-margin">
+                        <Col>
+                            <div className='font-weight-medium'> Requirements documents </div>
+                            {this.renderfilteredDocs('requirements')}
+                            <DocumentPicker showSelected={false} documentType='requirements' onChange={(docs) => { this.onChangeValue('requirements', docs)}}/>
+                        </Col>
+                    </Row>
+                    <Row className="documents-row-bottom-margin">
+                        <Col>
+                            <div className='font-weight-medium'> Other documents </div>
+                            {this.renderfilteredDocs('other')}
+                            <DocumentPicker showSelected={false} documentType='other' onChange={(docs) => { this.onChangeValue('other', docs)}}/>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
+
 }
